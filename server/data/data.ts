b5d9c44a6e63df50c8ff7e9f8af56460908f31eb
xWafl <73sampleperson@gmail.com>
2020-04-10 07:37:27 -0400

    :wrench: Get ready to deploy to heroku

--- server/data/data.ts
+++ server/data/data.ts
@@ -3,10 +3,11 @@ import { HeartbeatData } from "../helpers/heartbeats";
 
 const getData = async (userid: number, midnight: number): Promise<HeartbeatData[]> => {
     const prevMidnight = midnight - 86400000;
+    console.log(`${prevMidnight}|${midnight}`);
     return <HeartbeatData[]>await knex("events")
         .where({userid})
-        .where("timestart", "<", midnight)
-        .where("timeend", ">", prevMidnight)
+        .andWhere("timestart", "<", midnight)
+        .andWhere("timeend", ">", prevMidnight)
         .catch(e => {
             throw e;
         });
